47fbca525f4650185e958509397f80d607c94900
DLDLD <31480436+DLDLD@users.noreply.github.com>
2017-09-06 23:37:34 -0400

    Implemented missing sections for submission

--- listingController.js
+++ listingController.js
@@ -1,14 +1,22 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
-  function($scope, Listings) {
-    $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+	function($scope, Listings) {
+		$scope.listings = Listings;
+		$scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
-  }
+		$scope.addListing = function() {
+			$scope.listings.push($scope.newListing);
+			$scope.newListing = '';
+		};
+		
+		$scope.deleteListing = function(index) {
+			$scope.listings.splice(index, 1);
+			$scope.selectedIndex = -1;
+			$scope.detailedInfo = ' ';
+		};
+		
+		$scope.showDetails = function(index) {
+			$scope.detailedInfo = $scope.listings[index];
+			$scope.selectedIndex = index;
+		};
+	}
 ]);
